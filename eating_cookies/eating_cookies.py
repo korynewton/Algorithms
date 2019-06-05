dc6968e35e2ee9208bb8ec9aea77784b476e8d8f
Kory Newton <korybnewton@gmail.com>
2019-06-05 15:48:24 -0700

    eating cookies with cached values, WIP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,12 +8,6 @@ import sys
 
 
 def eating_cookies(n, cache=None):
-    # if n == 0:
-    #     return 0
-    # if n == 1:
-    #     return 1
-    # if n == 2:
-    #     return 2
     if n < 0:
         return 0
     elif n == 0:
@@ -22,10 +16,34 @@ def eating_cookies(n, cache=None):
         return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        num_cookies = int(sys.argv[1])
-        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
-            ways=eating_cookies(num_cookies), n=num_cookies))
+print(eating_cookies(1))
+print(eating_cookies(3))
+
+cookie_cache = {}
+
+
+def eating_cookies_cached(n, cookie_cache):
+    if n in cookie_cache:
+        return cookie_cache[n]
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
     else:
-        print('Usage: eating_cookies.py [num_cookies]')
+        number_cookies = eating_cookies_cached(
+            n-1, cookie_cache) + eating_cookies_cached(n-2, cookie_cache) + eating_cookies_cached(n-3, cookie_cache)
+    cookie_cache[n] = number_cookies
+    return number_cookies
+
+
+print(eating_cookies_cached(1, cookie_cache))
+print(eating_cookies_cached(3, cookie_cache))
+print(eating_cookies_cached(10, cookie_cache))
+print(eating_cookies_cached(50, cookie_cache))
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_cookies = int(sys.argv[1])
+#         print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+#             ways=eating_cookies(num_cookies), n=num_cookies))
+#     else:
+#         print('Usage: eating_cookies.py [num_cookies]')
