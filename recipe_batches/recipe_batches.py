afa536fa28aea4793ee2386e5e392cfb3633e7fa
Kory Newton <korybnewton@gmail.com>
2019-06-05 14:34:05 -0700

    completes recipes_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,31 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    limiting_factor = []
+    for key, value in recipe.items():
+        if key not in ingredients:
+            return 0
+        else:
+            limiting_factor.append(ingredients[key]//value)
+
+    limiting_factor.sort()
+    # print(sorted_list)
+
+    return limiting_factor[0]
+
+
+# print(recipe_batches({'milk': 100, 'butter': 50, 'cheese': 10}, {
+#       'milk': 198, 'butter': 52, 'cheese': 10}))
+# print(recipe_batches({'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5}, {
+#       'milk': 1288, 'flour': 9, 'sugar': 95}))
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
